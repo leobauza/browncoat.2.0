61d01734b5b89584992f734bd0d48cfcb1140e7e
Leo <lbauza@saforian.com>
2013-12-06 16:42:43 -0500

    add docs files

---
+++ views/_layouts.php
@@ -0,0 +1,8 @@
+<section class="container">
+	<h2>Layouts</h2>
+
+	<ol>
+		<li><a href="/layouts/fluidrows">Fluid Rows</a></li>
+		<li><a href="/layouts/staticsidebar">Static Sidebar</a></li>
+	</ol>
+</section>
