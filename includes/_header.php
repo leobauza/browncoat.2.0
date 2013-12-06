fd04a366d565273a7de40c9c0361d610a2492d04
Leo <lbauza@saforian.com>
2013-12-06 16:00:32 -0500

    add list styles and some views

--- includes/_header.php
+++ includes/_header.php
@@ -8,7 +8,7 @@
 	<meta name="author" content="the author">
 
 	<!-- styles -->
-	<link rel="stylesheet" href="../assets/css/styles.css" type="text/css" media="screen" charset="utf-8">
+	<link rel="stylesheet" href="/assets/css/styles.css" type="text/css" media="screen" charset="utf-8">
 	
 	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
 	<!--[if lt IE 9]>
@@ -21,5 +21,11 @@
 
 <body>
 	<header class="site-header container">
-		<h1>Browncoat 2.0</h1>
+		<h1><a href="/">Browncoat 2.0</a></h1>
 	</header>
+	<nav class="site-nav">
+		<!-- <li><a href="/css/forms">forms</a></li>
+		<li><a href="/css/lists">lists</a></li> -->
+		<li><a href="/css">css</a></li>
+		<li><a href="/libraries">libraries</a></li>
+	</nav>
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php include('./includes/_header.php'); ?>
+
 <?php
 
 require 'Slim/Slim.php';
@@ -6,13 +8,31 @@ $app = new Slim();
 
 
 $app -> get('/', function() {
-	echo "hello world!";
+	include './views/_home.php' ;
+});
+$app -> get('/libraries(/)', function() {
+	include './views/_libraries.php' ;
+});
+$app -> get('/css(/)', function() {
+	include './views/_css.php';
 });
 
-$app -> get('/something', function() {
-	echo "hello something!";
+
+$app -> get('/css/:id(/)', function($id) {
+
+	if($id == 'lists') {
+		include './views/_lists.php';
+	}
+	if($id == 'forms') {
+		include './views/_forms.php';
+	}
+
 });
+
+
 
 $app->run();
 
 ?>
+
+<?php include('./includes/_footer.php'); ?>
